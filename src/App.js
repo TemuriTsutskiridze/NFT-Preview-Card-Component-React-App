23915dd6c9d8137f57448206ba73d594d5522177
Temuri Tsutskiridze <temuri.tsutskiridze.04@gmail.com>
2023-03-13 17:46:52 +0400

    full project

--- src/App.js
+++ src/App.js
@@ -1,25 +1,41 @@
-import logo from './logo.svg';
 import './App.css';
+import './reset.css';
+import { ReactComponent as IconClock } from './images/icon-clock.svg';
+import { ReactComponent as IconEthereum } from './images/icon-ethereum.svg';
+import { ReactComponent as IconView } from './images/icon-view.svg';
+import imageAvatar from './images/image-avatar.png';
+import imageEquilibrium from './images/image-equilibrium.jpg';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='root'>
+      <div className='image-container'>
+        <img className = 'equilibrium' src = {imageEquilibrium} alt = "equilibrium image"/>
+        <div className='hover'>
+          <IconView className='view'/>
+        </div>
+      </div>
+
+      <h1 className='title'>Equilibrium #3429</h1>
+      <p className='text'>Our Equilibrium collection promotes balance and calm.</p>
+    
+      <div className='info'>
+        <div className='eth'>
+          <IconEthereum/>
+          <p>0.041 ETH</p>
+        </div>
+        <div className='time'>
+          <IconClock/>
+          <p>3 days left</p>
+        </div>
+      </div>
+
+      <div className='avatar'>
+        <img src = {imageAvatar} alt = "avatar"></img>
+        <p className='avatar-text'>Creation of <span>Jules Wyvern</span></p>
+      </div>
     </div>
-  );
+  )
 }
 
 export default App;
